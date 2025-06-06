c4d16a99e3e7b5fd17562e74463680e6cd8fcd54
Tomas Jose <ktjose813@gmail.com>
2025-06-06 13:27:49 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600">
+      Flashcard App (Vite + Tailwind + Firebase)
+    </div>
+  );
+}
+
+export default App
